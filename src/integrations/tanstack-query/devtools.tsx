053727953edf3c4f507ab896e409a95ef8367b8c
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-04-18 03:51:42 +1000

    fix: fixed linting issues

--- src/integrations/tanstack-query/devtools.tsx
+++ src/integrations/tanstack-query/devtools.tsx
@@ -1,6 +1,8 @@
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 
-export default {
+const tanstackQueryDevtools = {
 	name: "Tanstack Query",
 	render: <ReactQueryDevtoolsPanel />,
 };
+
+export default tanstackQueryDevtools;
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,5 +1,5 @@
 import { SignIn, useAuth } from "@clerk/clerk-react";
-import { Navigate, createFileRoute } from "@tanstack/react-router";
+import { Link, Navigate, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
 	component: LoginPage,
@@ -25,7 +25,7 @@ function LoginPage() {
 			<h1>Administrator login</h1>
 			<p>
 				Authorised access only. If you want to create a booking, please go to the{" "}
-				<a href="/book">booking page</a>. No login required.
+				<Link to="/book">booking page</Link>. No login required.
 			</p>
 			<SignIn
 				routing="hash"
