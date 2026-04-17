e85b38edc93b8058d065fb5729d9f29167621600
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-04-17 15:38:18 +1000

    feat: updated important message

--- src/content/booking.ts
+++ src/content/booking.ts
@@ -93,7 +93,7 @@ export const bookingStepOneContent: BookingStepOneContent = {
 	modalIframeTitle: "Choose a session",
 	postBookingNotice: {
 		title: "IMPORTANT",
-		body: "After picking session date, complete STEP 2 below or booking will be invalid.",
+		body: "After picking session date, close this popup and complete STEP 2 or booking will be invalid.",
 		dismissLabel: "I understand",
 	},
 	recurringBookingUrl,
