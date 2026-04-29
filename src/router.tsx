9bbcc36e0fe6685e0e9653c676dcd594c1b86480
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-04-30 01:55:35 +1000

    feat: disabled scroll restoration

--- src/router.tsx
+++ src/router.tsx
@@ -10,7 +10,7 @@ export function getRouter() {
 	const router = createTanStackRouter({
 		routeTree,
 		context,
-		scrollRestoration: true,
+		scrollRestoration: false,
 		defaultPreload: "intent",
 		defaultPreloadStaleTime: 0,
 	});
