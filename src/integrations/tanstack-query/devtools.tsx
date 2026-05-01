70512971db7928db10a5c213331866016664314c
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-05-02 03:23:06 +1000

    refactor: removed unused dependencies

--- src/integrations/tanstack-query/devtools.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
-
-const tanstackQueryDevtools = {
-	name: "Tanstack Query",
-	render: <ReactQueryDevtoolsPanel />,
-};
-
-export default tanstackQueryDevtools;
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import babel from "@rolldown/plugin-babel";
@@ -8,7 +7,6 @@ import { defineConfig } from "vite";
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
-		devtools(),
 		tailwindcss(),
 		tanstackStart({
 			prerender: {
